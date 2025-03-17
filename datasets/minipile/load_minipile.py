54a14e9b0a6e7c2c8a1ada8528535812c958cc0a
WillSH97 <williamsh1997@hotmail.com>
2025-03-17 16:20:56 +1000

    :clown_face: added down-sampling

--- datasets/minipile/load_minipile.py
+++ datasets/minipile/load_minipile.py
@@ -3,10 +3,18 @@ I'm only doing this for like being overly organised really.
 
 Stealing from here: https://huggingface.co/datasets/JeanKaddour/minipile
 
+sampling down to 10% to not blow up computer
+
 '''
 
 from datasets import load_dataset
+import random
+import numpy as np
 
 dataset = load_dataset("JeanKaddour/minipile", split="train")
 
-minipile_text = dataset["text"]
+random.seed(42)
+
+sample=random.sample(dataset["text"], k=int(0.03*float(len(dataset)))) #sample 30k rows
+
+minipile_text = " ".join(sample)
